12a13dd3cab99c0aa2dc6d552b41f1bed5c62f1c
Paweł Szulc <paul.szulc@gmail.com>
2016-03-05 12:11:30 +0100

    Add NothingTank

---
+++ Tanks.scala
@@ -0,0 +1,17 @@
+package rabbit
+
+import robocode.HitRobotEvent;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+import java.awt._
+
+class NothingTank extends Robot {
+
+  override def run: Unit = {  }
+
+  override def onHitRobot(ev: HitRobotEvent) =  {  }
+
+  override def onScannedRobot(ev: ScannedRobotEvent) = {}
+}
+
